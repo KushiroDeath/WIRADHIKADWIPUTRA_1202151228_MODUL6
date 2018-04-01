63df489c2689925fa28b974288da6992ef8cbbfb
KushiroDeath <wiradd@yahoo.com>
2018-04-01 20:28:31 +0700

    first commit

--- app/src/main/java/com/example/wiradhikadwiputra/wiradhikadwiputra_1202151228_modul6/DetailPostActivity.java
+++ app/src/main/java/com/example/wiradhikadwiputra/wiradhikadwiputra_1202151228_modul6/DetailPostActivity.java
@@ -15,14 +15,13 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.wiradhikadwiputra.wiradhikadwiputra_1202151228_modul6.adapter.CommentAdapter;
 import com.example.wiradhikadwiputra.wiradhikadwiputra_1202151228_modul6.model.User;
+import com.example.wiradhikadwiputra.wiradhikadwiputra_1202151228_modul6.model.Comment;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import org.w3c.dom.Comment;
 
 import java.util.ArrayList;
 
